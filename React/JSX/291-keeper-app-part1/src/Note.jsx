f5ca49146ff0a94660ef97b8c03082bb9fe1a658
kowsalyaThirugnanam <kowsalyavignesh1993@gmail.com>
2024-05-28 08:35:46 +0530

    react props

---
+++ React/JSX/291-keeper-app-part1/src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(params) {
+  return (
+    <div className="note">
+      <h1>This is a Note title</h1>
+      <p>This is a Note content</p>
+    </div>
+  );
+}
+
+export default Note;
