f105df3cfe64dd11df630a709fa8867e1598cdfc
kowsalyaThirugnanam <kowsalyavignesh1993@gmail.com>
2024-11-18 00:22:15 +0530

    Parent-child-data-pass

---
+++ React/JSX/317-Eg-Parent-Child-Data-Pass/src/components/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [noteList, setNoteList] = useState([]);
+
+  function addItem(item, event) {
+    console.log("item", item);
+    setNoteList((preValue) => {
+      return [...preValue, item];
+    });
+    event.preventDefault();
+    console.log("list", noteList);
+  }
+
+  function deleteItem(id) {
+    console.log("delete id clicked", id);
+    setNoteList((preValue) => {
+      return preValue.filter((item, index) => {
+        return index != id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea addItem={addItem} />
+      {noteList.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            deleteItem={deleteItem}
+          />
+        );
+      })}
+      {/* <Note key={1} title="Note title" content="Note content" /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/JSX/317-Eg-Parent-Child-Data-Pass/src/components/CreateArea.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [cardInput, setCardInput] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleInput(events) {
+    const { name, value } = events.target;
+    console.log("wht", name, value);
+    setCardInput((preValue) => {
+      return { ...preValue, [name]: value };
+    });
+    console.log("cardInput", cardInput);
+  }
+
+  return (
+    <div>
+      <form
+        onSubmit={() => {
+          props.addItem(cardInput, event);
+          setCardInput({
+            title: "",
+            content: "",
+          });
+        }}
+      >
+        <input
+          name="title"
+          placeholder="Title"
+          onChange={handleInput}
+          value={cardInput.title}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          onChange={handleInput}
+          value={cardInput.content}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
